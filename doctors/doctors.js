78686fd46219aa2d53a1ac62843c8f848577ee4a
TasZyb <87321674+TasZyb@users.noreply.github.com>
2023-01-12 23:34:18 +0200

    Add files via upload

---
+++ doctors/doctors.js
@@ -0,0 +1,45 @@
+let doctorsRow = document.querySelector(".doctors__row");
+const worker = [
+    {
+        "picture": "../img/1.JPG",
+        "name": "Врач 1",
+        "profesion": "Професія"
+    },
+    {
+        "picture": "../img/2.JPG",
+        "name": "Врач 1",
+        "profesion": "Професія"
+    },
+    {
+        "picture": "../img/1.JPG",
+        "name": "Врач 1",
+        "profesion": "Професія"
+    },
+    {
+        "picture": "../img/1.JPG",
+        "name": "Врач 1",
+        "profesion": "Професія"
+    },
+    {
+        "picture": "../img/1.JPG",
+        "name": "Врач 1",
+        "profesion": "Професія"
+    }
+]
+
+let html = '';
+
+worker.forEach(e => {
+    const item = `
+    <article class="doctors__article">
+        <div class="doctors__picture" style="background-image: url(${e.picture})">
+        </div>
+        <h5>${e.name}</h5>
+        <p>${e.profesion}</p>
+    </article>
+    `
+
+    html += item
+});
+
+doctorsRow.innerHTML = html 
--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-number = document.querySelector(".save__text");
-message = document.querySelector(".message");
-burger = document.querySelector(".header__burger");
-burger__list = document.querySelector(".header__nav");
-number.addEventListener("click", () => { 
-    navigator.clipboard.writeText(number.innerHTML);
-    message.style.animation = "";
-    setTimeout(function() {
-        message.style.animation = "mess 2.5s";
-      }, 1);
-});
+let number = document.querySelectorAll(".save__text");
+let message = document.querySelector(".message");
+const burger = document.querySelector(".header__burger");
+const burger__list = document.querySelector(".header__nav");
+const body = document.querySelector("body")
+
+
+
+number.forEach(e =>{
+    
+    e.addEventListener("click", () => { 
+        console.log(number);
+        navigator.clipboard.writeText(e.innerHTML);
+        message.style.animation = "";
+        setTimeout(function() {
+            message.style.animation = "mess 2.5s";
+          }, 1);
+    });
+})
+
+
 burger.addEventListener("click", () => { 
-    burger.classList.toggle("active");
+    burger.classList.toggle("active__burger");
     burger__list.classList.toggle("active__list");
+    body.classList.toggle("scroll__lock")
 });
+
 
 
