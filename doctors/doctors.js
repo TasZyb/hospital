07b7a51dada77d34cbda3b141b60150b3803c064
TasZyb <87321674+TasZyb@users.noreply.github.com>
2023-01-30 22:36:05 +0200

    Add files via upload

--- doctors/doctors.js
+++ doctors/doctors.js
@@ -1,32 +1,52 @@
-let doctorsRow = document.querySelector(".doctors__row");
+const doctorsRow = document.querySelector(".doctors__row");
+const popap = document.querySelector(".popap");
+
 
 
 
 const worker = [
     {
-        "picture": "../img/1.JPG",
-        "name": "Врач 1",
-        "profesion": "Професія"
+        id: "one",
+        picture: "../img/6.PNG",
+        name: "Діденко Оксана Зіновіївна",
+        profesion: "Лікар-Кардіолог",
+        info: "Стаж: 18 років. Кардіолог, терапевт вищої категорії. Кандидат медичних наук, асистент кафедри пропедевтики внутрішньої медицини ЛНМУ ім. Данила Галицького, автор 19 друкованих праць"
     },
     {
-        "picture": "../img/2.JPG",
-        "name": "Врач 1",
-        "profesion": "лікар"
+        id: "two",
+        picture: "../img/7.JPG",
+        name: "Діденко Тарас Васильович",
+        profesion: "Лікар-Уролог",
+        info: 'Стаж: 18 років. Уролог, лікар ультразвукової діагностики. Інтернатура, спец. "Хірургія" (2004 - 2006 рр., ЛНМУ ім. Данила Галицького). Ординатура, спец. "Урологія" (2010 - 2011 рр., ЛНМУ ім. Данила Галицького)'
+
     },
     {
-        "picture": "../img/1.JPG",
-        "name": "Врач 1",
-        "profesion": "Професія"
+        id: "three",
+        picture: "../img/8.JPG",
+        name: "Бліхарський Юрій Зіновійович",
+        profesion: "Професія",
+        info: ""
     },
     {
-        "picture": "../img/1.JPG",
-        "name": "Врач 1",
-        "profesion": "Професія"
+        id: "four",
+        picture: "../img/1.JPG",
+        name: "Ходань Оксана Андріївна",
+        profesion: "Лікар-Eндокринолог",
+        info: 'Лікар ендокринолог. Стаж роботи 15 років за спеціальністю ендокринологія. 2006-2008 рр. Інтернатура за спеціальністю внутрішні хвороби. 2009р. спеціалізація ендокринологія. Додаткова спеціалізація ультразвукова діагностика 2021рік.'
     },
     {
-        "picture": "../img/1.JPG",
-        "name": "Врач 1",
-        "profesion": "Професія"
+        id: "five",
+        picture: "../img/9.JPG",
+        name: "Музика Жанетта Антонівна",
+        profesion: "Лікар-УЗД",
+        info: " Лікар УЗД вищої категорії (стаж узд- 30 років)"
+    },
+    {
+        id: "six",
+        picture: "../img/10.JPG",
+        name: "Музика Жанетта Антонівна",
+        profesion: "Лікар-УЗД",
+        info: " Лікар УЗД вищої категорії (стаж узд- 30 років)"
     }
 ]
 
@@ -34,7 +54,7 @@ let htmlArticle = '';
 let info = '';
 worker.forEach(e => {
     const item = `
-    <article class="doctors__article">
+    <article class="doctors__article" id=${e.id}>
         <div class="doctors__picture" style="background-image: url(${e.picture})">
         </div>
         <h5>${e.name}</h5>
@@ -42,9 +62,37 @@ worker.forEach(e => {
     </article>
     `
 
-    htmlArticle += item
+    htmlArticle += item;
 });
 
-doctorsRow.innerHTML = htmlArticle 
-let article = document.querySelectorAll(".doctors__article")
-console.log(article);
+doctorsRow.innerHTML = htmlArticle;
+const generator = (obj) => (
+    `
+    <div class="popap__img" style="background-image: url(${obj.picture})"></div>
+    <h2 class="popap__title">${obj.name}</h2>
+    <h3 class="popap__second">${obj.profesion}</h3>
+    <p class="popap__text">${obj.info}</p>
+     `
+);
+
+// let article = document.querySelectorAll(".doctors__article");
+const doctorsClick = document.querySelectorAll(".doctors__article");
+const popupBg = document.querySelector(".back__opasity");
+let popapText = `ys`
+console.log(doctorsClick);
+doctorsClick.forEach(e => {
+    e.addEventListener('click', () => { // Вешаем обработчик на крестик
+        popap.innerHTML = generator(worker.find(i => i.id === e.id));
+        popap.classList.add('activePopap');
+        popupBg.classList.add('popupBg');
+    });
+})
+
+
+
+document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+        popap.classList.remove('activePopap');
+        popupBg.classList.remove('popupBg');
+    }
+});
--- map.js
+++ map.js
@@ -2,11 +2,11 @@ let maps;
 
 function initMap() {
   maps = new google.maps.Map(document.querySelector(".map"), {
-    center: { lat: 50.05743, lng:  23.96766 },
-    zoom: 15,
+    center: { lat: 50.0570774, lng:  23.97294676 },
+    zoom: 18,
   });
   let marker = new google.maps.Marker({
-    position: {lat: 50.05743, lng:  23.96766 },
+    position: {lat: 50.0570774, lng:  23.9729467 },
     map: maps,
     title: "Ми тут"
 
